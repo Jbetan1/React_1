c43e6242f10dfe4630b9b81c203e315c25f4303f
Jbetan1 <jos3betan1@gmail.com>
2022-12-08 15:01:02 -0300

    Agregando_react

---
+++ my-app/src/components/create.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+export default function Create() {
+    let history = useHistory();
+    const [nome, setNome] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [email, setEmail] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
+    console.log(checkbox)
+    const postData = () => {
+        axios.post(`http://localhost:3000/contactos`, {
+            nome,
+           telefone,
+           email,
+            checkbox
+        }).then(() => {
+            history.push('/read')
+        })
+    }
+    return (
+        <div>
+            <Form className="create-form">
+                <Form.Field>
+                    <label>nome</label>
+                    <input placeholder='nome' onChange={(e) => setNome(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>email</label>
+                    <input placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>telefone</label>
+                    <input placeholder='telefone' onChange={(e) => setTelefone(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
+                </Form.Field>
+                <Button onClick={postData} type='submit'>Submit</Button>
+            </Form>
+        </div>
+    )
+}
